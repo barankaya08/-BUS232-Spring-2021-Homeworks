18f5c65721c5aa1022340d89658fcf7674822674
barankaya08 <81385627+barankaya08@users.noreply.github.com>
2021-04-05 21:25:20 +0300

    Add files via upload

---
+++ BARAN KAYA.py
@@ -0,0 +1,10 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+BARAN KAYA  
+
+name = input('Hi i am a chatbot. Please state your name : ')
+print("Thank you "+name ) ,
+print("how can ı help you ? :")
+searching = input("")
+print("Searching for "+ searching) 
